386ec295ac2bbb4eb850dc80d8d7dbf3006ff625
nosilv <nosilv8@gmail.com>
2025-01-10 21:50:42 +0900

    improve: 사용자에게 다음명령번호를 입력받는 구역을 메소드로 분리
    개선대상과 결과
    사용자에게 명령을 입력받는 main메소드의 네번째 구역 → getNextSteps메소드로 분리함
    main 메소드는 총 30줄로 줄어듦
    
    resolves: #29

--- src/main/java/com/example/calculatorLv2/App.java
+++ src/main/java/com/example/calculatorLv2/App.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class App {
 
     // Stage1. 정상적인 숫자 입력이 들어올 때까지 사용자 입력을 받고, 정상 입력값을 출력하는 함수
-    private static int getInts(Scanner sc, int n){
+    private static int getInts(Scanner sc, int n) {
         // 정상적인 숫자를 할당할 변수
         int num;
 
         // n번째 값 입력받기
         while (true) {
-            System.out.print("\t["+n+"]번째 수를 입력하세요(0 혹은 자연수): "); // 개행하지 않을 것이므로 print 사용
+            System.out.print("\t[" + n + "]번째 수를 입력하세요(0 혹은 자연수): "); // 개행하지 않을 것이므로 print 사용
             String input = sc.nextLine(); // 한 줄 입력받기
 
             // 대소문자 구분없이 exit 입력시 프로그램 종료
@@ -21,8 +21,8 @@ public class App {
             }
             try {
                 num = Integer.parseInt(input);
-                if(num>=0) return num;
-                else{
+                if (num >= 0) return num;
+                else {
                     System.out.println("\t\t[ERR] 잘못된 입력: 입력값 '" + input + "'은(는) 음수입니다.");
                 }
             } catch (NumberFormatException e) {
@@ -64,6 +64,33 @@ public class App {
         }
     }
 
+    // Stage4. 정상적인 계산기 명령어 선택번호 입력이 들어올 때까지 사용자 입력을 받고, 정상 입력값을 출력하는 함수
+    private static int getNextSteps(Scanner sc) {
+        while (true) {
+            System.out.println("\t① '확인' : 가장 오래된 결과값 확인\t② '수정': 수정\t③ '제거': 제거\t④ 'exit' : 계산기 종료\t⑤ 그 외 모든 값 : 계산기 재시작");
+            System.out.print("\t다음 할 일을 번호나 단어로 입력하세요 >> ");
+            String input = sc.nextLine();  // 한 줄 입력받기
+
+            // 대소문자 구분없이 exit 입력시 프로그램 종료
+            if (input.equalsIgnoreCase("exit") || input.equals("4")) {
+                System.out.println("계산기를 종료합니다.");
+                System.exit(0);
+            }
+
+            // 정상적인 계산기 명령어 선택번호 입력시 입력값 리턴
+            switch (input) {
+                case "1", "확인":
+                    return 1;
+                case "2", "수정":
+                    return 2;
+                case "3", "삭제":
+                    return 3;
+                default:
+                    return 0;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);    // Scanner 객체 생성
         Calculator myCalculator = new Calculator();
@@ -77,15 +104,15 @@ public class App {
             // 1. 2개의 값을 입력받는 단계
             System.out.println("Stage 1. 0 혹은 자연수 2개 입력 받기");
             a = getInts(myScanner, 1);
-            b= getInts(myScanner, 2);
-                // 입력한 숫자 재확인
+            b = getInts(myScanner, 2);
+            // 입력한 숫자 재확인
             System.out.println("\t입력값: " + a + ", " + b);
 
             // 2. 사칙연산 기호를 입력받는 단계
             System.out.println("Stage 2. 사칙연산 기호 입력받기: 아래의 기호 혹은 선지번호를 입력하세요.");
             symbol = getSymbols(myScanner);
 
-            // 3. 연산 수행 : Calcuator클래스의 객체myCalculator 활용
+            // 3. 연산 수행 : Calcuator 클래스의 객체 myCalculator 활용
             System.out.println("Stage 3. 사칙연산 수행");
             System.out.print("\t" + a + ' ' + symbol + ' ' + b + " = ");
             try {
@@ -94,45 +121,46 @@ public class App {
                 System.out.println("[ERR] 잘못된 결과: " + e.getMessage());
             }
             // 4. 이후 할 일 선택 : 재시작할지/가장 오래된 결과값을 확인/수정/제거할지 확인
+            System.out.println("Stage 4. 다음 할 일 선택");
             while (true) {
-                System.out.println("Stage 4. 다음 할 일 선택");
-                System.out.println("\t① '확인' : 가장 오래된 결과값 확인\t② '수정': 수정\t③ '제거': 제거\t④ 'exit' : 계산기 종료\t⑤ 그 외 모든 값 : 계산기 재시작");
-                System.out.print("\t다음 할 일을 번호나 단어로 입력하세요 >> ");
-                input = myScanner.nextLine();  // 한 줄 입력받기
-
-                if (input.equalsIgnoreCase("exit") || input.equals("4")) {
-                    System.out.println("계산기를 종료합니다.");
-                    return;
-                } else if (input.equals("1") || input.equals("확인")) {
-                    System.out.print("\t가장 오래된 결과값: ");
-                    try {
-                        double temp = myCalculator.getResultOldest();
-                        System.out.println(temp);
-                    } catch (Exception e) {
-                        System.out.println("비어있습니다.");
-                    }
-                } else if (input.equals("2") || input.equals("수정")) {
-                    while (true) {
-                        System.out.print("\t가장 오래된 결과값을 어떤 값으로 수정할까요 >> ");
-                        if (myScanner.hasNextDouble()) {
-                            double temp = myScanner.nextDouble();
-                            myScanner.nextLine();   // if 문에서 읽었던 버퍼 비우기
-                            if (myCalculator.setResultOldest(temp))
-                                System.out.println("\t수정에 성공하였습니다.");
-                            else System.out.println("\t비어있습니다. 수정에 실패하였습니다.");
-                            break;
-                        } else {
-                            myScanner.nextLine();   // if 문에서 읽었던 버퍼 비우기
-                            System.out.println("\t\t[Err] 정상적인 숫자값을 입력해주세요");
+                int menu = getNextSteps(myScanner);
+                switch (menu) {
+                    case 1:
+                        System.out.print("\t가장 오래된 결과값: ");
+                        try {
+                            double temp = myCalculator.getResultOldest();
+                            System.out.println(temp);
+                        } catch (Exception e) {
+                            System.out.println("비어있습니다.");
                         }
-                    }
-                } else if (input.equals("3") || input.equals("제거")) {
-                    System.out.print("\t가장 오래된 결과값을 제거합니다 : ");
-                    if (myCalculator.deleteResultOldest()) {
-                        System.out.println("\t성공하였습니다.");
-                    } else
-                        System.out.println("\t비어있습니다. 제거에 실패하였습니다.");
-                } else break;
+                        break;
+                    case 2:
+                        while (true) {
+                            System.out.print("\t가장 오래된 결과값을 어떤 값으로 수정할까요 >> ");
+                            if (myScanner.hasNextDouble()) {
+                                double temp = myScanner.nextDouble();
+                                myScanner.nextLine();   // if 문에서 읽었던 버퍼 비우기
+                                if (myCalculator.setResultOldest(temp))
+                                    System.out.println("\t수정에 성공하였습니다.");
+                                else System.out.println("\t비어있습니다. 수정에 실패하였습니다.");
+                                break;
+                            } else {
+                                myScanner.nextLine();   // if 문에서 읽었던 버퍼 비우기
+                                System.out.println("\t\t[Err] 정상적인 숫자값을 입력해주세요");
+                            }
+                        }
+                        break;
+                    case 3:
+                        System.out.print("\t가장 오래된 결과값을 제거합니다 : ");
+                        if (myCalculator.deleteResultOldest()) {
+                            System.out.println("\t성공하였습니다.");
+                        } else
+                            System.out.println("\t비어있습니다. 제거에 실패하였습니다.");
+                        break;
+                    case 0:
+                        break;
+                }
+                if(menu==0) break;
             }
         }
     }
